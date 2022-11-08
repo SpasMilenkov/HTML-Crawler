59fcc5a206ae6d3790af4481f83bf989d2b62843
Spas Milenkov <spasmilenkov123@gmail.com>
2022-11-08 20:45:25 +0200

    Introduce Parser class instance and remove parsing functions from Program.cs

--- HTML Crawler Prototype/HTML Crawler Prototype/Program.cs
+++ HTML Crawler Prototype/HTML Crawler Prototype/Program.cs
@@ -20,20 +20,9 @@ namespace HTML_Crawler_Prototype
         
         //HARDCODED REMOVE LATER
         private static string _Path = "/home/spasmilenkov/Documents/SAA-uni/small-test.txt";
-        //the reason for my existence
-        private static GTree<string> HtmlTree = new GTree<string>();
-        //allows access to all string manipulation commands
-        private static Helper _helper = new Helper();
-        static string _html = "";
-        //checks if a tag is closed
-        private static bool _closed = false;
-        //checks if a tag is opened
-        private static bool _openTag = false;
-        //keeps the total number of opened tags
-        private static int _totalOpened = 0;
-        //keeps the total number of closed tags
-        private static int _totalClosed = 0;
-        
+        //HTML Parsing instance
+        private static Parser _parser = new Parser();
+
         public static void Main(string[] args)
         {
             LoadUi();
@@ -85,7 +74,7 @@ namespace HTML_Crawler_Prototype
 
                     while ((line = sr.ReadLine()) != null)
                     {
-                        _html = _html += line;
+                        _parser.Html = _parser.Html += line;
                         Console.WriteLine(line);
                     }
                 }
@@ -98,102 +87,18 @@ namespace HTML_Crawler_Prototype
                 Console.WriteLine(e.Message);
             }
             // create the tree s first node
-            string[] data = GetOpeningTag(_html);
+            string[] data = _parser.GetOpeningTag(_parser.Html);
             string props = "";
             for (int i = 1; i < data.Length; i++)
                 props += data[i];
-            _html = _helper.Slice(_html, data[0].Length + 2);
-            string value = GetValue(_html, data[0]);
-            value = _helper.Slice(_html, 0, _html.Length - (data[0].Length + 3));
-            HtmlTree.SetGTree(data[0], "", value, null);
-            _html.Substring(0);
-            ParseHtml(value, HtmlTree);
+            _parser.Html = Helper.Slice(_parser.Html, data[0].Length + 2);
+            string value = _parser.GetValue(_parser.Html, data[0]);
+            value = Helper.Slice(_parser.Html, 0, _parser.Html.Length - (data[0].Length + 3));
+            _parser.HtmlTree.SetGTree(data[0], "", value, null);
+            _parser.Html.Substring(0);
+            _parser.ParseHtml(value, _parser.HtmlTree);
         }
-        public static void ParseHtml(string html, GTree<string> parent)
-         {
-             if (html.Length == 0)
-             {
-                 return;
-             }
-
-             html = _helper.Trim(html);
-             string[] data = GetOpeningTag(html);
-             string props = "";
-             for (int i = 1; i < data.Length; i++)
-                 props += data[i];
-             
-             html = _helper.Slice(html, data[0].Length + 2);
-             string value = GetValue(html, data[0]);
-
-             html = _helper.Slice(html, 0, html.Length - (data[0].Length+3));
-
-             if (_closed == true)
-             {
-                 GTree<string> node = new GTree<string>();
-                 node.SetGTree(data[0], props, value, parent);
-                 parent.AddChild(node);
-                 ParseHtml(html, node);
-             }
-             else
-                Console.WriteLine("HTML parsing error");
-             
-         }
-        public static string[] GetOpeningTag(string html)
-        {
-            //start looking < and > to get the opening html tag
-            string nodeData = "";
-
-            for (int j = 0; j < html.Length; j++)
-            {
-                char c = html[j];
-                
-                if (c == '>')
-                {
-                    _openTag = false;
-                    return _helper.Split(nodeData, ' ');
-                }
-                
-                if (_openTag)
-                    nodeData += c;
-                
-                if (c == '<')
-                    _openTag = true;
-            }
-            throw new Exception("Internal Error");
-        }
-
-        public static string GetValue(string text, string tag)
-        {   
-            string val = "";
-            for (int j = 0; j < text.Length; j++)
-            {
-                char c = text[j];
-                if (c == '<' && text[j + 1] == '/')
-                {
-                    string closingTag = GetClosingTag(text, j+2);
-                    if (tag == closingTag)
-                    {
-                        _closed = true;
-                        return val;
-                    } 
-                }
-                val += c;
-            }
-            return val;
-        }
-        public static string GetClosingTag(string html, int i)
-        {
-            char c = html[i];
-            string tag = "";
-            while (c != '>' && i <html.Length - 1)
-            {
-                tag += c;
-                i++;
-                c = html[i];
-            }
-
-            return tag;
-        }
+        
          public static void PrintNode()
          {
              
