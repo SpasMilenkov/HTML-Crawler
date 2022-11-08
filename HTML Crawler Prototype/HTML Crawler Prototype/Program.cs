a15c8e6ac5a984a728aa3da1c56e8da41a02dd7d
Spas Milenkov <spasmilenkov123@gmail.com>
2022-11-08 21:10:38 +0200

    Organize code in directories

--- HTML Crawler Prototype/HTML Crawler Prototype/Program.cs
+++ HTML Crawler Prototype/HTML Crawler Prototype/Program.cs
@@ -3,7 +3,7 @@
 //with the passing of time however now only God knows
 //after you are done bumping your head in the wall trying to fix this 
 //increment the counter below so the next dreaded individual knows what awaits him 
-//hours wasted on this: 18
+//hours wasted on this: 19
 
 using System.Diagnostics;
 
@@ -11,18 +11,11 @@ namespace HTML_Crawler_Prototype
 {
     public class Program 
     {
-        //<div> </div>
-        //<img></img>
-        //<img/>
-        //<br/>
-        //<br> </br>
-        //<div/>
+        //the simple command line interface rendering
         private static UiHandler _menu = new UiHandler();
         public static void Main(string[] args)
         {
             _menu.LoadUi();
         }
-        //the simple command line interface rendering
-
     }
 }
