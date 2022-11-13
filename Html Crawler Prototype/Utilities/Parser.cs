c0e68b92f131373dd518a65cee387f3bab2ac399
Spas Milenkov <spasmilenkov123@gmail.com>
2022-11-13 22:21:32 +0200

    Rewrite the parsing algorithm

--- Html Crawler Prototype/Utilities/Parser.cs
+++ Html Crawler Prototype/Utilities/Parser.cs
@@ -5,7 +5,7 @@ public class Parser
 {
     //the reason for my existence
     public GTree<string> HtmlTree = new GTree<string>();
-    public static string Html = "";
+    public string Html = "";
     //checks if a tag is closed
     public bool Closed = false;
     //checks if a tag is opened
@@ -16,80 +16,63 @@ public class Parser
     public int TotalClosed = 0;
     //keeps temporary indexing data
     private static int _indexer = 0;
-    public void ParseHtml(int begin, int end, GTree<string> parent)
+    public void ParseHtml()
     {
-        if (begin >= end)
-        {
-                 return;
-        }
-
-        // html = Helper.Trim(html);
-        string[] data = GetOpeningTag(begin, end);
-        string props = "";
-        for (int i = 1; i < data.Length; i++)
-            props += data[i];
-             
-        // html = Helper.Slice(html, data[0].Length + 2);
-        string value = GetValue(_indexer, data[0]);
-
-        // html = Helper.Slice(html, 0, html.Length - (data[0].Length+3));
-        if (Closed)
-        {
-            GTree<string> node = new GTree<string>();
-            node.SetGTree(data[0], props, value, parent);
-            parent.AddChild(node);
-            // ParseHtml(html, node);
-        }
-        else
-            Console.WriteLine("HTML parsing error");
-             
-    }
-    public string[] GetOpeningTag(int begin, int end)
-    {
-        //start looking < and > to get the opening html tag
-        string nodeData = "";
-
-        for (int i = begin; i < end; i++)
+        for (int i = 0; i < Html.Length; i++)
         {
             char c = Html[i];
-                
-            if (c == '>')
-            {
-                Closed = false;
-                i++;
-                _indexer = i;
-                return Helper.Split(nodeData, ' ');
-            }
-                
-            if (Closed)
-                nodeData += c;
-                
             if (c == '<')
-                Closed = true;
+            {
+                GetOpeningTag(HtmlTree);
+            }
         }
-        throw new Exception("Opening Tag Error");
+    }
+    public void GetOpeningTag(GTree<string> currentNode)
+    {
+        if(currentNode == null || _indexer >= Html.Length - 1)
+            return;
+        _indexer++;
+        string nodeValue = "";
+        char c = Html[_indexer];
+        while (c != '>' && _indexer < Html.Length-1)
+        {
+            nodeValue += c;
+            _indexer++;
+            c = Html[_indexer];
+        }
+        string[] props = Helper.Split(nodeValue, ' ');
+        currentNode.Tag = props[0];
+        Helper.Slice(props, 1, props.Length);
+        GetValue(currentNode);
+
     }
 
-    public string GetValue(int begin, string tag)
-    {   
-        string val = "";
-        for (int i = begin; i < Html.Length -1 ; i++)
+    public void GetValue(GTree<string> currentNode)
+    {
+        if (_indexer >= Html.Length - 1)
+            return;
+        _indexer++;
+        char c = Html[_indexer];
+        string textValue = "";
+        while (c != '<' && _indexer < Html.Length-1)
         {
-            char c = Html[i];
-            if (c == '<' && Html[i + 1] == '/')
-            {
-                GetClosingTag(i + 2);
-                // if (tag == closingTag)
-                // {
-                //     Closed = true;
-                //     return val;
-                // } 
-            }
-            val += c;
+            textValue += c;
+            _indexer++;
+            c = Html[_indexer];
         }
-        return val;
+        if (Html[_indexer + 1] == '/')
+        {
+            currentNode.Value = textValue;
+            TotalClosed++;
+            GetOpeningTag(currentNode.Parent);
+        }
+        GTree<string> child = new GTree<string>();
+        child.Parent = currentNode;
+        currentNode.AddChild(child);
+        GetOpeningTag(child);
+
     }
-    public static void GetClosingTag(int begin)
+    public void GetClosingTag(int begin)
     {
         int i = begin;
         for (; i < Html.Length; i++)
