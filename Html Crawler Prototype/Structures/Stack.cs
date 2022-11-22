8c54000e094b3f036e0feb84df0b6c6435ad6b4e
Spas Milenkov <spasmilenkov123@gmail.com>
2022-11-22 23:41:45 +0200

    Add stack structure

---
+++ Html Crawler Prototype/Structures/Stack.cs
@@ -0,0 +1,21 @@
+namespace HTML_Crawler_Prototype;
+
+public class Stack<T>
+{
+    private List<T> _container;
+    
+    public void Push(T value)
+    {
+        _container.Add(value);
+    }
+
+    public void Pop()
+    {
+        if (!IsEmpty())
+            throw new Exception("Stack underflow");
+        
+        var lastEl = _container[_container.Count - 1];
+        _container.RemoveAt(_container.Count - 1);
+    }
+    public bool IsEmpty() => _container.Count == 0;
+}
