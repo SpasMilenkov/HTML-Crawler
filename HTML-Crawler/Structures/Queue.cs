e75b7de1b9111ced7ea2f3ec8045ca9e9ba1aa75
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2022-12-08 15:45:24 +0200

    Update the Queue Class

--- HTML-Crawler/Structures/Queue.cs
+++ HTML-Crawler/Structures/Queue.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel;
+
 namespace HTML_Crawler_Prototype;
 
-public class Queue<T>
+public class MyQueue<T>
 {
     private List<T> _container = new List<T>();
-    
+
     public void Enqueue(T value)
     {
         _container.Add(value);
@@ -13,10 +15,17 @@ public class Queue<T>
     {
         if (IsEmpty())
             throw new Exception("Queue underflow");
-        
+
         var firstEl = _container[0];
         _container.RemoveAt(0);
         return firstEl;
     }
-    public bool IsEmpty() =>  _container.Count == 0;
+    public T? Peek()
+    {
+        T t = default(T);
+        if (IsEmpty())
+            return t;
+        return _container[0];
+    }
+    public bool IsEmpty() => _container.Count == 0;
 }
