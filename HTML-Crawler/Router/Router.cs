0fcea00f0ba6ab6c6663205a5f514faad018958b
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2022-12-08 15:48:02 +0200

    Add the router class

---
+++ HTML-Crawler/Router/Router.cs
@@ -0,0 +1,39 @@
+﻿using HTML_Crawler_Prototype;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTML_Crawler.Router
+{
+    internal class Router
+    {
+        public void ParseInput(string input)
+        {
+            string command = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                {
+                    Helper.Slice(input, i);
+                    break;
+                }
+                command += input[i];
+            }
+
+            switch (command)
+            {
+                case "PRINT":
+                    break;
+                case "SET":
+                    break;
+                case "Copy":
+                    break;
+                case "//":
+                    break;
+            }
+        }
+    }
+}
