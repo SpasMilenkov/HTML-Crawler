96a6c78ac1d8bc224562a1b5de3b7af955a5e193
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2022-12-10 19:04:34 +0200

    Add constructor for cloning a node

--- HTML-Crawler/Structures/GTree.cs
+++ HTML-Crawler/Structures/GTree.cs
@@ -7,17 +7,39 @@ public class GTree<T>
     //holds the node tag
     public T Tag { get; set; }
     //holds the node s props (id, class)
+    //TODO: Convert to linked list
     public List<string> Props = new List<string>();
     //keep the connection to the parent element
     public GTree<T> Parent { get; set; }
     public string Value { get; set; }
     public int Depth { get; set; }
-    public bool Visited = false;
     public bool SelfClosing = false;
     //hold the child nodes that are connected to this one
     //using linked list is necessary because the html DOM is not binary tree
     //the amount of child nodes are not limited to 2
     public MyLinkedList<GTree<T>> _childNodes = new MyLinkedList<GTree<T>>();
+
+    public GTree()
+    {
+
+    }
+
+    public GTree (GTree<T> newNode)
+    {
+        Tag = newNode.Tag;  
+        Parent = newNode.Parent;
+        Value = newNode.Value;
+        Depth = newNode.Depth;
+        SelfClosing = newNode.SelfClosing;
+        var child = newNode._childNodes.First();
+
+        while( child != null)
+        {
+            this.AddChild(child.Value);
+            child = child.Next;
+        }
+    }
+
 
     //adds new child to the child nodes list
     public void AddChild(GTree<T> child)
