d4e2318443f43b1798437a96f08d9b0b3e9fea13
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2022-12-13 20:38:50 +0200

    Add a wrapper class

---
+++ HTML-Crawler/Structures/Wrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTML_Crawler.Structures
+{
+    public class Wrapper<T>
+    {
+        public T Value;
+        public int Depth;
+
+        public Wrapper(T node,int depth)
+        {
+            Value = node;
+            Depth = depth;
+        }
+    }
+}
